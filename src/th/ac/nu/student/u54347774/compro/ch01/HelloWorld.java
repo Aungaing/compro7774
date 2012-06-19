c4c31b0fb24f186bb0e650517d2aaa08d906ec7c
Aungaing <abienegr@hotmail.com>
2012-06-19 14:38:33 +0700

    เพิ่ม Programming is fun

--- src/th/ac/nu/student/u54347774/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/student/u54347774/compro/ch01/HelloWorld.java
@@ -11,5 +11,6 @@ package th.ac.nu.student.u54347774.compro.ch01;
 public class HelloWorld {
     public static void main(String [] args){
     System.out.println("Hello,World");
+    System.out.println("Programming is fun.");
     }
 }

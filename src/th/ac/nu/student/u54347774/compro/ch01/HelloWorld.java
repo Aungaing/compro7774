3cddea01f5f8ca097fc70aae96c858f78d7a9054
Aungaing <abienegr@hotmail.com>
2012-06-19 14:33:07 +0700

    สร้าง HelloWorld

---
+++ src/th/ac/nu/student/u54347774/compro/ch01/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54347774.compro.ch01;
+
+/**
+ *
+ * @author Ienovo
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+    System.out.println("Hello,World");
+    }
+}
